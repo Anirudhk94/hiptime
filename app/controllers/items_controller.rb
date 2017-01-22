4b79e133ca3d20e15fead771901c656a462cbbf1
Anirudh Kulkarni <k.anirudh8@gmail.com>
2017-01-22 14:33:28 +0530

    Added logic for creating item

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,26 @@
 class ItemsController < ApplicationController
+	def index
+		
+	end
+
+	def new
+		@item = Item.new
+	end
+
+	def create
+		@item= Item.new(item_params)
+		if @item.save
+			redirect_to root_path
+		else
+			render 'new'
+		end
+		
+	end
+
+
+	private
+
+	def item_params
+		@item = params.require(:item).permit(:title, :description)
+	end
 end
