16b0b97c330b61d1f6ffb4f4a1128978da2b5e2b
Anirudh Kulkarni <k.anirudh8@gmail.com>
2017-01-22 23:26:32 +0530

    UI changes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,9 @@
 class Item < ApplicationRecord
 	belongs_to :user
 
+	validates :title, presence: true, length: { minimum: 5}
+	validates :description, presence: true
+
 	def completed?
 		!completed_at.blank?
 	end
